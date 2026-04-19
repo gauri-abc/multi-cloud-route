26b5dc71ef2cfcc489bb9cbd1219449422ee4686
Gauri yadav <67154990+gauri-abc@users.noreply.github.com>
2026-04-19 12:35:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require("path");
 
 const app = express();
 app.set("trust proxy", true);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // ── Middleware ────────────────────────────────────────────────
 app.use(cors());
